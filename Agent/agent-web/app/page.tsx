5f159f361ad4a8437bd30e2d5d3b2b8516c35209
AndrewThien <anretrithien@gmail.com>
2026-01-14 14:06:57 +0000

    update auth

--- Agent/agent-web/app/page.tsx
+++ Agent/agent-web/app/page.tsx
@@ -4,6 +4,7 @@ import { AuthButton } from "@/components/auth-button";
 
 export default async function Home() {
   const session = await auth.api.getSession({ headers: await headers() });
+  console.log(session);
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
--- Agent/agent-web/lib/auth.ts
+++ Agent/agent-web/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
-import { genericOAuth, keycloak } from "better-auth/plugins";
+import { genericOAuth } from "better-auth/plugins";
+import jwt from "jsonwebtoken";
 
 const baseURL =
   process.env.BETTER_AUTH_URL ||
@@ -12,11 +13,32 @@ export const auth = betterAuth({
   plugins: [
     genericOAuth({
       config: [
-        keycloak({
-          clientId: process.env.KEYCLOAK_CLIENT_ID || "",
-          clientSecret: process.env.KEYCLOAK_CLIENT_SECRET || "",
-          issuer: process.env.KEYCLOAK_ISSUER || "",
-        }),
+        {
+          providerId: "keycloak",
+          clientId: process.env.KEYCLOAK_CLIENT_ID!,
+          clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
+          authorizationUrl: `${process.env.KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}/`,
+          scopes: ["openid"],
+          getUserInfo: async (tokens) => {
+            // Access provider-specific fields from raw token data
+            const decodedToken = jwt.decode(tokens.accessToken!) as any;
+            return {
+              id: decodedToken.sub,
+              name: decodedToken.name,
+              email: decodedToken.email,
+              emailVerified: decodedToken.email_verified,
+              roles: decodedToken.roles,
+              permissions: decodedToken.permissions,
+              groups: decodedToken.groups,
+              clientRoles: decodedToken.clientRoles,
+              realmRoles: decodedToken.realmRoles,
+              clientId: decodedToken.clientId,
+              clientName: decodedToken.clientName,
+              clientDisplayName: decodedToken.clientDisplayName,
+              clientIcon: decodedToken.clientIcon,
+            };
+          },
+        },
       ],
     }),
   ],
